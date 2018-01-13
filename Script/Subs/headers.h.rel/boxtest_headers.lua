7740788624ce8868d22c797de8f1be4c89977e74
Tricky1975 <facebook@tbbs.nl>
2018-01-13 23:08:15 +0100

    Boxtext Headers
    (And a little readme file explaining the name and nature of headers.h)

---
+++ Script/Subs/headers.h.rel/boxtest_headers.lua
@@ -0,0 +1,28 @@
+--[[
+  boxtest_headers.lua
+  
+  version: 18.01.13
+  Copyright (C) 2018 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+function LoadScenario(tag,file)
+   -- $USE Libs/scenario
+   assert(tag,"No tag for LoadScenario")
+   assert(file,"No file for LoadScenario")
+   return scenario.LoadData(file,tag)
+end   
+
+
+return true
---
+++ Script/Subs/headers.h.rel/readme.md
@@ -0,0 +1,13 @@
+# headers.h
+
+The name of this library is a pun to the large collection of .h files ever C programmer hates.
+It's very unsatisfying work to create these files just for the sake of code cleanness and good interfacing of your libraries.
+
+Now this library is not exactly the same but also hated a bit.
+The game is a bit written with the 'load on demand' idea in mind. This being said things would go quicker to have a command ready to which immediately loads the required library when it's needed for the first time.
+And that is basically what all functions here do. 
+
+Nice work to set these files up? Defintely not!
+But saving me a lot of trouble later? Definitely yes!
+And that's why I still need this.
+
