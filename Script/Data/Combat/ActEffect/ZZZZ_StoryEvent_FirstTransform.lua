287c5ee74991222270baaf069ef439108596b674
Tricky1975 <facebook@tbbs.nl>
2018-06-23 16:40:55 +0200

    This should fix #109.. At least... I hope it does :P

--- Script/Data/Combat/ActEffect/ZZZZ_StoryEvent_FirstTransform.lua
+++ Script/Data/Combat/ActEffect/ZZZZ_StoryEvent_FirstTransform.lua
@@ -32,7 +32,7 @@
   
  **********************************************
  
-version: 18.04.21
+version: 18.06.23
 ]]
 
 return function(s,tar,exe,a)
@@ -44,6 +44,7 @@ return function(s,tar,exe,a)
   s:RyannaTransform('Lucifer')
   MapText("TUT_TRANS")
   gamedata.learned = gamedata.learned or {}
+  gamedata.learned.Ryanna = gamedata.learned.Ryanna or {}
   gamedata.learned.Ryanna.Access = true
   rpg:Points("FOE_1","HP").Minimum=0
   return true
