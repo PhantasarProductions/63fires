dd230eddef7a3bf404ce3ce0a6029e3b347bf765
Tricky1975 <facebook@tbbs.nl>
2018-07-05 20:51:11 +0200

    Puzzle generation
    Please note, the map does not yet call it, so at this moment all it does it taking up place, but as soon as the map has properly set up, this code will have its function ;)

--- Script/Maps/Script/DUNG_Starrow.lua
+++ Script/Maps/Script/DUNG_Starrow.lua
@@ -32,9 +32,99 @@
   
  **********************************************
  
-version: 18.06.16
+version: 18.07.05
 ]]
 local sta = {}
+local primes = {
+  2,
+  3,
+  5,
+  7,
+  11,
+  13,
+  17,
+  19,
+  23,
+  29,
+  31,
+  37,
+  41,
+  43,
+  47,
+  53,
+  59,
+  61,
+  67,
+  71,
+  73,
+  79,
+  83,
+  89,
+  97,
+}
+
+local puzgens = {}
+local puzseq
+math.randomseed(os.time())
+local rnd = math.random
+
+function puzgens:add()
+   puzseq = {}
+   local av = rnd(1,10)
+   for i=1,5 do puzseq[i]=i*av end
+end
+
+function puzgens:sub()
+   puzseq = {}
+   local av = rnd(1,10)
+   for i=1,5 do puzseq[6-i]=i*av end
+end
+
+function puzgens:mul()
+   puzseq = {}
+   local av = rnd(1,10)
+   local vv = rnd(1,10)
+   for i=1,5 do vv=vv*av puzseq[i]=v end
+end
+
+function puzgens:prime()
+   puzseq = {}
+   local sp = rnd(1,#primes-5)
+   for i=1,5 do puzseq[i]=primes[i+sp] end
+end
+
+function puzgens:addsub()
+   local a1=rnd(3,20)
+   local b1=rnd(1,a1-1)
+   local v=rnd(5,25)
+   puzseq = {}
+   for i=1,5 do 
+       if i==1 or i==3 or i==3 then v = v + a1 else v = v - b1 end
+       puzseq[i]=v
+   end
+end   
+        
+
+local puzskill = {
+     {"add","sub"},
+     {"add","sub","mul","prime"},
+     {"add","sub","mul","prime","addsub"}
+}
+
+local puzzlesolved="&DONE.CAVESOFSTARROW.SEQUENCEPUZZLE.SOLVED"
+local skill = Var.G("%SKILL")
+
+field:ZA_Enter("PUZZLE_GEN",function()
+   if Var.C(puzzlesolved)=="TRUE" then return end
+   if puzseq then return end
+   local ps = puzskill[skill]
+   puzgens[ps[rnd(1,#1)]](puzgens)
+end)
+
+field:ZA_Enter("PUZZLE_FORGET",function()
+  if Var.C(puzzlesolved)=='TRUE' then return end
+  puzseq=nil
+end)
 
 function sta:NPC_Gauntlet()
    CSay("Hoera! We hebben een gauntlet gevonden!")
