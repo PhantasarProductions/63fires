0db493160b8192b9b582f341c52950de4e8f8dc7
Tricky1975 <facebook@tbbs.nl>
2018-12-01 19:23:02 +0100

    Hook Jump

--- Script/Flows/field.rel/haken.lua
+++ Script/Flows/field.rel/haken.lua
@@ -1,3 +1,39 @@
+--[[
+  haken.lua
+  Version: 18.12.01
+  Copyright (C) 2018 Jeroen Petrus Broks
+  
+  ===========================
+  This file is part of a project related to the Phantasar Chronicles or another
+  series or saga which is property of Jeroen P. Broks.
+  This means that it may contain references to a story-line plus characters
+  which are property of Jeroen Broks. These references may only be distributed
+  along with an unmodified version of the game. 
+  
+  As soon as you remove or replace ALL references to the storyline or character
+  references, or any termology specifically set up for the Phantasar universe,
+  or any other univers a story of Jeroen P. Broks is set up for,
+  the restrictions of this file are removed and will automatically become
+  zLib licensed (see below).
+  
+  Please note that doing so counts as a modification and must be marked as such
+  in accordance to the zLib license.
+  ===========================
+  zLib license terms:
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
 local haken = {}
 
 -- "haken" means "hooks" in Dutch.
@@ -23,8 +59,51 @@ function haken:initHookSpots()
     return total
 end
 
+-- "Slave" is a regular term in the world of computers, so any "slave" activists, put the axes down.... slowly! 
+local function HideSlaves()
+   local map = field:GetMap()
+   for i=2,4 do
+       local slave = map.map.TagMap[map.layer][("PLAYER%d"):format(i)]
+       if slave then slave.VISIBLE=false end
+   end
+end
+
+local function ShowSlaves(adjust)
+   local map = field:GetMap()
+   local master = map.map:obj(map.layer,"PLAYER1")
+   for i=2,4 do
+       local slave = map.map.TagMap[map.layer][("PLAYER%d"):format(i)]
+       if slave then 
+          slave.VISIBLE=true 
+          if adjust then
+             slave.COORD.x=master.COORD.x
+             slave.COORD.y=master.COORD.y
+          end   
+       end
+   end
+end
+
+
+
 local function HookCross(self,coords)
-    CSay("Jumping not yet done! Please come back later!")
+    --CSay("Jumping not yet done! Please come back later!")
+    HideSlaves()
+    local map = self:getmap()
+    local Ryanna = map.map:obj(map.layer,"PLAYER1")
+    repeat
+          if Ryanna.COORD.x<coords.x then Ryanna.COORD.x=Ryanna.COORD.x+4 end
+          if Ryanna.COORD.x>coords.x then Ryanna.COORD.x=Ryanna.COORD.x-4 end
+          if Ryanna.COORD.y<coords.y then Ryanna.COORD.y=Ryanna.COORD.y+4 end
+          if Ryanna.COORD.y>coords.y then Ryanna.COORD.y=Ryanna.COORD.y-4 end
+          self:autoscroll()
+          kthura.drawmap(map.map,map.layer,self.cam.x,self.cam.y)
+          StatusBar(false,false)
+          showstrip()
+          Color(0,180,255)
+          love.graphics.line(Ryanna.COORD.x-map.cam.x,(Ryanna.COORD.y-32)-map.cam.y,coords.x-map.cam.x,(coords.y-16)-map.cam.y)
+          love.graphics.present()                   
+    until math.abs(Ryanna.COORD.x-coords.x)<60 and math.abs(Ryanna.COORD.y-coords.y)<60       
+    ShowSlaves(true)
 end
 
 function haken:findClosestHookSpot()
