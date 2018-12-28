146b115c6f3986787a883eafc572d2261463b1d7
Tricky1975 <facebook@tbbs.nl>
2018-12-28 19:52:59 +0100

    Bugfixes in the absorb routine. This should now all work fine!

--- Script/Data/Combat/ActEffect/Absorb.lua
+++ Script/Data/Combat/ActEffect/Absorb.lua
@@ -54,8 +54,8 @@ return function(combat,targettag,shirleytag,params)
    local absorb = ShirleyAbsorb -- LAAAAZY!
    
    -- Data shorcuts
-   local sal = gamedata.Shirley.AbsorbList    sal[target.ufil] = sal[target.ufil] or 0
-   local sac = gamedata.Shirley.AbsorbCount   sac[target.ufil] = sac[target.ufil] or 0
+   local sal = gamedata.xchardata.Shirley.AbsorbList    sal[target.ufil] = sal[target.ufil] or 0
+   local sac = gamedata.xchardata.Shirley.AbsorbCount   sac[target.ufil] = sac[target.ufil] or 0
    local ufil = target.ufil   
    
    -- HP
@@ -73,9 +73,19 @@ return function(combat,targettag,shirleytag,params)
    if success then
       sac[ufil] = sac[ufil] + 1
       if sal[ufil]<fhp then sal[ufil]=fhp end
-      combat:TagMessage(target,"Absorbed!",255,36,0)
-      combat.fighters[target]=nil
-      combat.foes[target]=nil
+      combat:TagMessage(targettag,"Absorbed!",255,36,0)
+      combat.fighters[targettag]=nil
+      combat.foes[targettag]=nil
+      combat.foedraworder={}
+      local orderkill={}
+      for k,myfoe in spairs(combat.foedrawordertag) do
+          if myfoe.tag==targettag then 
+             orderkill[#orderkill+1]=k
+          else   
+             combat.foedraworder[#combat.foedraworder+1]=myfoe
+          end    
+      end
+      for k in each(orderkill) do combat.foedrawordertag[k]=nil end                       
       return true
    end   
    
@@ -83,6 +93,7 @@ return function(combat,targettag,shirleytag,params)
    if math.random(1,10)<=failpc[skill] then sac[ufil] = sac[ufil] + 1 end
    local verschil = srol-frol
    combat:Hurt("Shirley",verschil)
-   combat.TagMessage(target,"Absorb Resisted!",255,34,0)
-      
+   combat.TagMessage(targettag,"Absorb Resisted!",255,34,0)
+   return true
+   
 end
